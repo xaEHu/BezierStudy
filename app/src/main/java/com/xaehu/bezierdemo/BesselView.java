6b4433c242cacc1eecfc5006481d2c7c87a044c0
xaeHu <hsfemail@qq.com>
2019-08-01 13:58:33 +0800

    贝塞尔曲线的学习+自定义贝塞尔曲线小动画demo

---
+++ app/src/main/java/com/xaehu/bezierdemo/BesselView.java
@@ -0,0 +1,134 @@
+package com.xaehu.bezierdemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * @author : xaeHu
+ * e-mail : hsfemail@qq.com
+ * @date : 2019/7/31 10:54
+ * desc   :
+ */
+public class BesselView extends View {
+
+    private final String TAG = "myout";
+    private final int POINT_START = 0;
+    private final int POINT_CENTER = 3;
+    private final int POINT_END = 1;
+    private final int POINT_CTRL = 2;
+    private Paint paint;
+    private Paint linePaint;
+    private float startX, startY, endX, endY, ctrlX, ctrlY;
+    private float r;
+    private float touchX,touchY;
+    private Path path;
+    private int currentPoint = -1;
+
+    public BesselView(Context context) {
+        this(context,null);
+    }
+
+    public BesselView(Context context, AttributeSet attributeSet) {
+        super(context,attributeSet);
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        linePaint = new Paint();
+        linePaint.setAntiAlias(true);
+        linePaint.setStrokeWidth(3);
+        linePaint.setStyle(Paint.Style.STROKE);
+        startX = 100;
+        startY = 200;
+        endX = 1000;
+        endY = 200;
+        ctrlX = 800;
+        ctrlY = 500;
+        touchX = 300;
+        touchY = 500;
+        r = 10;
+        path = new Path();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        path.reset();
+        path.moveTo(startX, startY);
+        path.cubicTo(touchX,touchY, ctrlX, ctrlY, endX, endY);
+        canvas.drawPath(path,linePaint);
+        if(currentPoint != -1){
+            paint.setColor(ContextCompat.getColor(getContext(),R.color.colorPrimaryDark));
+            canvas.drawLine(startX, startY,touchX,touchY,paint);
+            canvas.drawLine(endX, endY, ctrlX, ctrlY,paint);
+            canvas.drawLine(ctrlX, ctrlY,touchX,touchY,paint);
+        }
+        paint.setColor(ContextCompat.getColor(getContext(),R.color.colorPrimary));
+        canvas.drawCircle(startX, startY,r,paint);
+        canvas.drawCircle(endX, endY,r,paint);
+        paint.setColor(ContextCompat.getColor(getContext(),R.color.colorAccent));
+        canvas.drawCircle(ctrlX, ctrlY,r,paint);
+        canvas.drawCircle(touchX,touchY,r,paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            float x = event.getX();
+            float y = event.getY();
+            if(isClickStart(x,y)){
+                currentPoint = POINT_START;
+            }else if(isClickEnd(x,y)){
+                currentPoint = POINT_END;
+            }else if(isClickCenter(x,y)){
+                currentPoint = POINT_CENTER;
+            }else if(isClickCtrl(x,y)){
+                currentPoint = POINT_CTRL;
+            }else{
+                currentPoint = -1;
+                invalidate();
+            }
+        }
+        if(event.getAction() == MotionEvent.ACTION_MOVE){
+            if(currentPoint != -1){
+                if(currentPoint == POINT_START){
+                    startX = event.getX();
+                    startY = event.getY();
+                }else if(currentPoint == POINT_END){
+                    endX = event.getX();
+                    endY = event.getY();
+                }else if(currentPoint == POINT_CENTER){
+                    ctrlX = event.getX();
+                    ctrlY = event.getY();
+                }else{
+                    touchX = event.getX();
+                    touchY = event.getY();
+                }
+                invalidate();
+            }
+        }
+        return true;
+    }
+
+    private boolean isClickCenter(float clickX, float clickY) {
+        return isClick(ctrlX, ctrlY,r,clickX,clickY);
+    }
+    private boolean isClickStart(float clickX,float clickY){
+        return isClick(startX, startY,r,clickX,clickY);
+    }
+    private boolean isClickEnd(float clickX,float clickY){
+        return isClick(endX, endY,r,clickX,clickY);
+    }
+    private boolean isClickCtrl(float clickX,float clickY){
+        return isClick(touchX,touchY,r,clickX,clickY);
+    }
+    private boolean isClick(float cx,float cy,float cr,float clickX,float clickY){
+        return  clickX < cx + cr
+                && clickX > cx - cr
+                && clickY < cy + cr
+                && clickY > cy - cr;
+    }
+}
---
+++ app/src/main/java/com/xaehu/bezierdemo/LoadinngView.java
@@ -0,0 +1,215 @@
+package com.xaehu.bezierdemo;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+
+/**
+ * @author : xaeHu
+ * e-mail : hsfemail@qq.com
+ * @date : 2019/7/30 14:48
+ * desc  :
+ */
+public class LoadinngView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+
+    private final int STATE_DOWN = 0;
+    private final int STATE_UP = 1;
+    private final int STATE_FREE = 2;
+    private int loadingState = STATE_DOWN;
+    private SurfaceHolder holder;
+    private boolean isRun;
+    private Paint ballPaint;
+    private Paint linePaint;
+    private Path path;
+
+    private ValueAnimator downAnimator;
+    private ValueAnimator upAnimator;
+    private ValueAnimator freeAnimator;
+    private AnimatorSet animatorSet;
+
+    //线宽
+    private float lineWidth;
+    //小球偏离水平线距离
+    private float distance;
+    private float distance_temp;
+    private float freeDownDistance;
+    //半径
+    private float r;
+
+    public LoadinngView(Context context) {
+        this(context, null);
+    }
+
+    public LoadinngView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        holder = this.getHolder();
+        holder.addCallback(this);
+        this.setFocusable(true);
+        path = new Path();
+        ballPaint = new Paint();
+        ballPaint.setAntiAlias(true);
+        linePaint = new Paint();
+        linePaint.setAntiAlias(true);
+        linePaint.setStrokeWidth(3);
+        linePaint.setStyle(Paint.Style.STROKE);
+        lineWidth = 300;
+        distance = 50;
+        r = 20;
+
+        initControl();
+    }
+
+    private void initControl() {
+        downAnimator = ValueAnimator.ofFloat(0, 1);
+        downAnimator.setDuration(500);
+        //减速
+        downAnimator.setInterpolator(new DecelerateInterpolator());
+        downAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                distance_temp = distance * (float) animation.getAnimatedValue();
+            }
+        });
+        downAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                loadingState = STATE_DOWN;
+            }
+        });
+        upAnimator = ValueAnimator.ofFloat(0, 1);
+        upAnimator.setDuration(500);
+        //加速
+        upAnimator.setInterpolator(new ShockInterpolator());
+        upAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                distance_temp = distance - distance * (float) animation.getAnimatedValue();
+                if (distance_temp <= 0 && !freeAnimator.isStarted()) {
+                    freeAnimator.start();
+                }
+            }
+        });
+        upAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                loadingState = STATE_UP;
+
+            }
+        });
+        freeAnimator = ValueAnimator.ofFloat(0, (float) (2 * Math.sqrt(distance / 5)));
+        freeAnimator.setDuration(600);
+        freeAnimator.setInterpolator(new LinearInterpolator());
+        freeAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float t = (float) animation.getAnimatedValue();
+                freeDownDistance = (float) (10 * Math.sqrt(distance / 5) * t - 5 * t * t);
+            }
+        });
+        freeAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                loadingState = STATE_FREE;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animatorSet.start();
+            }
+        });
+        animatorSet = new AnimatorSet();
+        animatorSet.play(downAnimator).before(upAnimator);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        isRun = true;
+        drawLoadingView();
+        new Thread(this).start();
+        animatorSet.start();
+    }
+
+    private void drawLoadingView() {
+        Canvas canvas = holder.lockCanvas();
+        try {
+            if (canvas != null) {
+                canvas.drawColor(Color.WHITE);
+                //贝塞尔曲线 + 中间小球
+                ballPaint.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+                path.reset();
+                path.moveTo((float) (getWidth() / 2) - lineWidth / 2, (float) (getHeight() / 2));
+                if (loadingState == STATE_DOWN) {
+                    path.rQuadTo(lineWidth / 2, distance_temp * 2, lineWidth, 0);
+                    canvas.drawCircle((float) (getWidth() / 2), (float) (getHeight() / 2) + distance_temp - r, r, ballPaint);
+                } else if (loadingState == STATE_UP) {
+                    path.rQuadTo(lineWidth / 2, distance_temp * 2, lineWidth, 0);
+                    canvas.drawCircle((float) (getWidth() / 2), (float) (getHeight() / 2) + distance_temp - r, r, ballPaint);
+                } else if (loadingState == STATE_FREE) {
+                    path.rQuadTo(lineWidth / 2, distance_temp * 2, lineWidth, 0);
+                    canvas.drawCircle((float) (getWidth() / 2), (float) (getHeight() / 2) - freeDownDistance - r, r, ballPaint);
+                }
+                canvas.drawPath(path, linePaint);
+                //两边小球
+                ballPaint.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+                canvas.drawCircle((float) (getWidth() / 2) - lineWidth / 2, (float) (getHeight() / 2), r, ballPaint);
+                canvas.drawCircle((float) (getWidth() / 2) + lineWidth / 2, (float) (getHeight() / 2), r, ballPaint);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            holder.unlockCanvasAndPost(canvas);
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        isRun = false;
+        if (animatorSet.isStarted()) {
+            animatorSet.end();
+            animatorSet.cancel();
+        }
+        if (freeAnimator.isStarted()) {
+            freeAnimator.end();
+            freeAnimator.cancel();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (isRun) {
+            drawLoadingView();
+        }
+    }
+
+    class ShockInterpolator implements Interpolator {
+
+        @Override
+        public float getInterpolation(float input) {
+            return (float) (1 - Math.exp(-3 * input) * Math.cos(10 * input));
+        }
+    }
+}
